03ffcf7fdff18d284ee879cf9e084b3c492b971b
davidelunetta <davidelunetta.27@gmail.com>
2023-11-29 14:56:26 +0100

    milestone completed

--- Partials/index.php
+++ Partials/index.php
@@ -6,6 +6,19 @@
     <title>PHP Hotels</title>
 </head>
 <body>
+    <?php
+    include "../Models/db.php";
+    ?>
+    <?php
+     foreach ($hotels as $hotel) {
+        echo '<h2>' . $hotel['name'] . '</h2>';
+        echo '<p>' . $hotel['description'] . '</p>';
+        echo '<p>Parcheggio disponibile: ' . ($hotel['parking'] ? 'Sì' : 'No') . '</p>';
+        echo '<p>Voto: ' . $hotel['vote'] . '</p>';
+        echo '<p>Distanza dal centro: ' . $hotel['distance_to_center'] . ' km</p>';
+        echo '<hr>';
+     }
+    ?>
     
 </body>
 </html>
